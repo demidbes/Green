0d8d2dd1f22696d802b613088cb40d3459a1a3f5
Дмитрий <dmitrii8155@icloud.com>
2025-07-06 18:51:38 +0300

    Верстка блока pre-sale + доработка предыдущих блоков

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// Бургер меню
 document.addEventListener('DOMContentLoaded', function() {
     const burger = document.querySelector('.header__burger-button')
     const nav = document.querySelector('.header__menu')
@@ -6,4 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
         this.classList.toggle('active');
         nav.classList.toggle('hidden-desktop');
     })
+})
+
+// Копирование текста
+
+document.querySelector('#copyButton').addEventListener('click', function() {
+    const range = document.createRange()
+    range.selectNode(document.querySelector('#copyText'))
+    window.getSelection().removeAllRanges()
+    window.getSelection().addRange(range)
+    document.execCommand('copy')
+    window.getSelection().removeAllRanges()
 })
